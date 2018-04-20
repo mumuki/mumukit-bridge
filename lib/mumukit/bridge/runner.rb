f139cf6b3bf3b60199d79d2cd310501355f5dcb2
Franco Bulgarelli <franco@mumuki.org>
2018-04-20 11:12:03 -0300

    All methods now accept headers

--- lib/mumukit/bridge/runner.rb
+++ lib/mumukit/bridge/runner.rb
@@ -22,8 +22,8 @@ module Mumukit
       #    status: :passed|:failed|:errored|:aborted|:passed_with_warnings,
       #    expectation_results: [{binding:string, inspection:string, result:symbol}],
       #    feedback: string}
-      def run_tests!(request)
-        with_server_response request, 'test' do |response|
+      def run_tests!(request, headers={})
+        with_server_response request, 'test', headers do |response|
           response_type = ResponseType.for_response response
           response_type.parse response
         end
@@ -34,14 +34,14 @@ module Mumukit
       # Returns a hash
       #   {result: string,
       #    status: :passed|:failed|:errored|:aborted}
-      def run_query!(request)
-        with_server_response request, 'query' do |it|
+      def run_query!(request, headers={})
+        with_server_response request, 'query', headers do |it|
           {status: it['exit'].to_sym, result: it['out']}
         end
       end
 
-      def run_try!(request)
-        with_server_response request, 'try' do |it|
+      def run_try!(request, headers={})
+        with_server_response request, 'try', headers do |it|
           {
             status: it['exit'].to_sym,
             result: it['out'],
@@ -53,8 +53,8 @@ module Mumukit
         end
       end
 
-      def importable_info
-        @language_json ||= info.merge('url' => test_runner_url)
+      def importable_info(headers={})
+        @language_json ||= info(headers).merge('url' => test_runner_url)
         {
           name:                   @language_json['name'],
           comment_type:           @language_json['comment_type'],
@@ -80,35 +80,35 @@ module Mumukit
         }
       end
 
-      def assets(path, options={})
-        do_get "assets/#{path}", options
+      def assets(path, headers={})
+        do_get "assets/#{path}", headers
       end
 
-      def info(options={})
-        JSON.parse do_get(:info, options.merge(content_type: :json))
+      def info(headers={})
+        JSON.parse do_get(:info, headers.merge(content_type: :json))
       end
 
       private
 
-      def with_server_response(request, route, &action)
-        response = do_post(route, request)
+      def with_server_response(request, route, headers, &action)
+        response = do_post(route, request, headers)
         action.call(response)
       rescue Exception => e
         {result: e.message, status: :aborted}
       end
 
-      def do_get(route, options)
-        RestClient.get("#{test_runner_url}/#{route}", options)
+      def do_get(route, headers)
+        RestClient.get("#{test_runner_url}/#{route}", headers)
       end
 
-      def do_post(route, request)
+      def do_post(route, request, headers)
         JSON.parse RestClient::Request.new(
                        method: :post,
                        url: "#{test_runner_url}/#{route}",
                        payload: request.to_json,
                        timeout: @timeout,
                        open_timeout: @timeout,
-                       headers: {content_type: :json}).execute()
+                       headers: headers.merge(content_type: :json)).execute
       end
 
       def get_assets_for(kind, content_type)
